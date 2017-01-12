80d14f70b2aecfd41ec1197b2161d603a75f4eab
1000141 <1000141@ONE1000141PN001.onestorecorp.com>
2017-01-12 16:05:30 +0900

    주석삽입

--- bjoj/src/_02252_lineup.java
+++ bjoj/src/_02252_lineup.java
@@ -1,10 +1,11 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 import java.util.Scanner;
 
-public class _2252_lineup {
+public class _02252_lineup {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -13,48 +14,52 @@ public class _2252_lineup {
 		int numOfComparision = sc.nextInt();
 		sc.nextLine();
 		
-		ArrayList<ArrayList<Integer>> edgeList = new ArrayList<ArrayList<Integer>> (numOfPerson+1);
+		List<Integer> [] edgeList = (List<Integer>[]) new List[numOfPerson+1];
 		int [] indegreeCnt = new int[numOfPerson+1];
 		
+		// 간선 리스트 선언
 		for( int i = 0; i <= numOfPerson; ++i )
 		{
-			edgeList.add(new ArrayList<Integer> ());
+			edgeList[i] = new ArrayList<Integer>();
 		}
 		
+		// 각 비교 데이터를 가지고 간선 리스트 초기화
 		for( int i = 0; i < numOfComparision; ++i )
 		{
 			int startPersonIndex = sc.nextInt();
 			int endPersonIndex = sc.nextInt();			
 			
-			edgeList.get(startPersonIndex).add(endPersonIndex);
+			edgeList[startPersonIndex].add(endPersonIndex);
 			indegreeCnt[endPersonIndex]++;
-			//sc.nextLine();
 		}
 		
-		for( ArrayList<Integer> curEdgeList : edgeList )
+		// 간선 오름차순 정렬 
+		for( List<Integer> curEdgeList : edgeList )
 			Collections.sort(curEdgeList);
 		
+		// 각 Person 별로 in-degree count 가 0인 Person을 Queue에 삽입
 		Queue<Integer> orderQueue = new LinkedList<Integer> ();
 		for( int i = 1; i <= numOfPerson; ++i )
 		{
 			if( indegreeCnt[i] == 0 )
-				orderQueue.offer(i);
+				orderQueue.add(i);
 		}
 		
+		// Queue 가 빌 때까지 진행
 		while( orderQueue.peek() != null )
 		{
-			int curPersonIndex = orderQueue.poll();
+			// Queue 에 들어가 있는 순서대로 in-degree count를 조사
+			int curPersonIndex = orderQueue.remove();
 			System.out.print(curPersonIndex + " ");
 			
-			if( edgeList.get(curPersonIndex) != null )
+			for( int curEndPersonIndex : edgeList[curPersonIndex] )
 			{
-				for( int curEndPersonIndex : edgeList.get(curPersonIndex) )
-				{
-					indegreeCnt[curEndPersonIndex]--;
-					
-					if( indegreeCnt[curEndPersonIndex] <= 0 )
-						orderQueue.offer(curEndPersonIndex);
-				}
+				// 비교 대상이 되는 Person의 in-degree count를 감소
+				indegreeCnt[curEndPersonIndex]--;
+				
+				// in-degree count가 0인 Person을 Queue 에 삽입
+				if( indegreeCnt[curEndPersonIndex] <= 0 )
+					orderQueue.add(curEndPersonIndex);
 			}
 		}
 	}
